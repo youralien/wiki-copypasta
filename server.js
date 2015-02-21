268a6333e0dc633c715150d6e972813b139697c0
themythicaldrago <ryan.louie@students.olin.edu>
2015-02-21 15:30:04 -0500

    enabled mongoose connections instead of reading & saving from file

--- server.js
+++ server.js
@@ -20,29 +20,21 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var comment = require('./routes/comment');
 var app = express();
+
+// mongoose database
+var mongoURI = process.env.MONGOURI || "mongodb://localhost/test";
+mongoose.connect(mongoURI);
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/comments.json', function(req, res) {
-  fs.readFile('_comments.json', function(err, data) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(data);
-  });
-});
-
-app.post('/comments.json', function(req, res) {
-  fs.readFile('_comments.json', function(err, data) {
-    var comments = JSON.parse(data);
-    comments.push(req.body);
-    fs.writeFile('_comments.json', JSON.stringify(comments, null, 4), function(err) {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify(comments));
-    });
-  });
-});
+app.get('/comments', comment.list);
+app.post('/comments', comment.add);
 
 app.listen(3000);
 
