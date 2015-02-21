8c40cfcfed8b95ae669e10230c8e1a184c83e924
themythicaldrago <ryan.louie@students.olin.edu>
2015-02-21 14:44:34 -0500

    added createdOn default field

--- models/Comment.js
+++ models/Comment.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var commentSchema = new mongoose.Schema({
 	author: String,
 	text: String,
+	createdOn: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('comment', commentSchema);
