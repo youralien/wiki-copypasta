1f34074d4df1dcf53d0c87407be096e15499293f
alias <email-blocked>
2015-02-23 22:47:54 -0500

    Modified Index and App.jsx to include Pasta-specific content.

--- components/Index.jsx
+++ components/Index.jsx
@@ -1,9 +1,29 @@
+/*
+* Default content route in accordance with routes.jsx
+* Contains HTML welcome mat for entrance to Pastapedia
+* and link to edit page (as prompt to create new article)
+*/
+
 'use strict';
 var React = require('react');
+var Router        = require('react-router');
+var DocumentTitle = require('react-document-title');
+
 
 var Index = React.createClass({
   render: function () {
-    return <p>Please select a place from the left.</p>;
+    return (
+      <DocumentTitle title="Pastapedia, the Copypasta Wiki">
+        <div className = "index">
+        	<div className = "indextitle">
+	        	<h2>Welcome to Pastapedia!</h2>
+        	</div>
+        	<div className = "indexcontent">
+	        	<p>Come for the copy, stay for the pastas.</p>
+        	</div>
+        </div>
+      </DocumentTitle>
+    );
   }
 });
 
