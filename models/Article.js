35e39773b902743c8e89943d1166f9244689cfb0
themythicaldrago <ryan.louie@students.olin.edu>
2015-02-26 14:49:02 -0500

    deleted Article.js

--- models/Article.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var articleSchema = new mongoose.Schema({
-	name: String,
-	text: String,
-	tags: [String]
-});
-
-module.exports = mongoose.model('wikiarticle', articleSchema);
