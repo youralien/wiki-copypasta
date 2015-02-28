522816ba128c1fdf2ddbde02703408f660e6407e
alias <email-blocked>
2015-02-27 19:17:32 -0500

    Rendering Ugly Article data, with no NoArticle Page

--- components/Article.jsx
+++ components/Article.jsx
@@ -10,25 +10,30 @@ var React         = require('react');
 var Router        = require('react-router');
 var DocumentTitle = require('react-document-title');
 
-var NoArticle     = require('./NoArticle.jsx'); 
+var Link          = Router.Link;
+
+// var Async         = require('react-async')
+// var XMLHttpRequest= require("xmlhttprequest").XMLHttpRequest;
+// var xhr = new XMLHttpRequest();
+// var url = "json";
+
+// var NoArticle     = require('./NoArticle.jsx'); 
 
 
 //TODO: Change this to correctly query database
-function findWikiPage(id) {
-  var forcedarticle = {name: id, text: "Hi Fred", tags: ["Fred","Hi"]};
-  return null
-}
+// function findWikiPage(id) {
+//   var forcedarticle = {name: id, text: "Hi Fred", tags: ["Fred","Hi"]};
+//   return forcedarticle
+// }
 
 var Article = React.createClass({
-  mixins: [ Router.State ], //used to get id of article, so as to find the correct article to render.
-
-  loadCommentsFromServer: function() {
+  mixins: [ Router.State ],
+  loadArticlesFromServer: function() {
     $.ajax({
-      url: "json",
+      url: '/article/'+this.getParams().id+'/json',
       dataType: 'json',
       success: function(data) {
         this.setState({data: data});
-        this.render()
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
@@ -39,51 +44,93 @@ var Article = React.createClass({
     return {data: {}};
   },
   componentDidMount: function() {
-    this.loadCommentsFromServer();
-    setInterval(this.loadCommentsFromServer, 2000); //TODO set 2000 dynamically, eg with this.props.pollInterval
+    this.loadArticlesFromServer();
+    // setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   },
-  componentWillMont: function() {
-    this.data = {"article": null};
-  },
-
-  render: function () {
-
-    
-    var article = this.data;//findWikiPage(this.getParams().id);
-
-    console.log(article);
-
-    if (!article) return <NoArticle />; //instead renders page as "no article found", etc., in accordance with NoArticle.jsx
-
-    // if (article.)
-
-    var tags = article.tags.map(function (tag) {
-      return (
-        <li key={"tag-" + tag}>
-          { tag }
-        </li>
-      );
-    });
-
-    return(
-      <DocumentTitle title ={ article.name }>
-        <div className = "article">
+  render: function() {
+    var links = "boo"
+    if (this.state.data) {
+      console.log(this.state.data)
+      links = this.state.data
+    }
+    else {
+      links = "We couldn't find your data"
+    }
+    // this.state.data.map(function(article) {
+    //   return (
+    //     <li>
+    //       <Link to="article" params={{id: article.name}}>{article.name}</Link>
+    //     </li>
+    //   );
+    // });
+    return (
+      <DocumentTitle title ={ this.getParams().id+" - Pastapedia" }>
+        <div className="article">
           <div className="articletitle">
-            <h2>{ article.name }</h2>
+            <h2>{ this.getParams().id }</h2>
           </div>
-          <div className = "tagtitle">
-            Tags:
-          </div>
-          <div className = "tags">
-            { tags }
-          </div>
-          <div className="articlecontent">
-            <p>{ article.text }</p>
-          </div>
+          <p>{links}</p>
         </div>
       </DocumentTitle>
     );
   }
 });
+//   mixins: [ Router.State ], //used to get id of article, so as to find the correct article to render.
+
+//   loadArticlesFromServer: function() {
+//     $.ajax({
+//       url: 'json',
+//       dataType: 'json',
+//       success: function(data) {
+//         this.setState({data: data});
+//       }.bind(this),
+//       error: function(xhr, status, err) {
+//         console.error(this.props.url, status, err.toString());
+//       }.bind(this)
+//     });
+//   },
+//   getInitialState: function() {
+//     return {data: {name: this.getParams().id, text: "No Article found", tags: []}};
+//   },
+//   componentDidMount: function() {
+//     console.log("did mount")
+//     this.loadArticleFromServer();
+//     setInterval(this.loadArticleFromServer, 2000);
+//   },
+
+//   render: function () {
+
+//     console.log(this.state.data);//return <h2>No Article Found!</h2>// <NoArticle />; //instead renders page as "no article found", etc., in accordance with NoArticle.jsx
+
+//     // if (article.)
+
+//     var tags = this.state.data.tags.map(function (tag) {
+//       return (
+//         <li key={"tag-" + tag}>
+//           { tag }
+//         </li>
+//       );
+//     });
+
+//     return(
+//       <DocumentTitle title ={ this.state.data.name }>
+//         <div className = "article">
+//           <div className="articletitle">
+//             <h2>{ this.state.data.name }</h2>
+//           </div>
+//           <div className = "tagtitle">
+//             Tags:
+//           </div>
+//           <div className = "tags">
+//             { tags }
+//           </div>
+//           <div className="articlecontent">
+//             <p>{ this.state.data.text }</p>
+//           </div>
+//         </div>
+//       </DocumentTitle>
+//     );
+//   }
+// });
 
 module.exports = Article;
