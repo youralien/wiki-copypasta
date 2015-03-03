38e3a9d4953ef197f579f53c16deb74fd5423201
themythicaldrago <ryan.louie@students.olin.edu>
2015-03-02 21:52:57 -0500

    added documentation for client.jsx

--- client.jsx
+++ client.jsx
@@ -1,3 +1,16 @@
+/*
+client.jsx
+
+In the ecosystem of an isomorphic react framework, client.jsx is the highest
+level module in the hierarchy of client-side React and React-Router files and
+components.  
+
+After server-side rendering of the static components happens, client.jsx will
+add event listeners once the DOM content is loaded in a re-rendering step.
+This adds the dynamic reactivity one would expect to all the server-side
+rendered components.
+ */
+
 /* global document */
 "use strict";
 /** @jsx React.DOM */
